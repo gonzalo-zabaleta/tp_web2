73b724543d8a3633325a717a58245bb349f513f9
Gonzalo Zabaleta <gonzalo.zabaleta15@gmail.com>
2024-10-16 18:19:43 -0300

    Modificacion completa de producto Controller

--- controllers/productoController.php
+++ controllers/productoController.php
@@ -1,58 +1,71 @@
 <?php
+require_once 'models/productoModel.php';
 require_once 'models/almacenModel.php';
 
-class AlmacenController {
+class ProductoController {
     private $model;
+    private $almacenModel;
 
     public function __construct() {
-        $this->model = new AlmacenModel();
+        $this->model = new ProductoModel();
+        $this->almacenModel = new AlmacenModel();
     }
 
     public function index() {
-        $almacenes = $this->model->getAllAlmacenes();
-        require 'views/almacen/list.phtml';
+        $productos = $this->model->getAllProductos();
+        require 'views/producto/list.phtml';
     }
 
     public function detail($id) {
-        $almacen = $this->model->getAlmacenById($id);
-        require 'views/almacen/detail.phtml';
+        $producto = $this->model->getProductoById($id);
+        require 'views/producto/detail.phtml';
     }
 
     public function add() {
+        $almacenes = $this->almacenModel->getAllAlmacenes();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $nombre = $_POST['nombre'];
-            $lugar = $_POST['lugar'];
-            $capacidad = $_POST['capacidad'];
-            if ($this->model->addAlmacen($nombre, $lugar, $capacidad)) {
-                header('Location: ' . BASE_URL . '/almacenes');
+            $cantidad = $_POST['cantidad'];
+            $color = $_POST['color'];
+            $idAlmacen = $_POST['id_almacen'];
+            if ($this->model->addProducto($nombre, $cantidad, $color, $idAlmacen)) {
+                header('Location: ' . BASE_URL . '/productos');
             } else {
-                $error = "Error al agregar el almacén";
+                $error = "Error al agregar el producto";
             }
         }
-        require 'views/almacen/add.phtml';
+        require 'views/producto/add.phtml';
     }
 
     public function edit($id) {
-        $almacen = $this->model->getAlmacenById($id);
+        $producto = $this->model->getProductoById($id);
+        $almacenes = $this->almacenModel->getAllAlmacenes();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $nombre = $_POST['nombre'];
-            $lugar = $_POST['lugar'];
-            $capacidad = $_POST['capacidad'];
-            if ($this->model->updateAlmacen($id, $nombre, $lugar, $capacidad)) {
-                header('Location: ' . BASE_URL . '/almacenes');
+            $cantidad = $_POST['cantidad'];
+            $color = $_POST['color'];
+            $idAlmacen = $_POST['id_almacen'];
+            if ($this->model->updateProducto($id, $nombre, $cantidad, $color, $idAlmacen)) {
+                header('Location: ' . BASE_URL . '/productos');
             } else {
-                $error = "Error al actualizar el almacén";
+                $error = "Error al actualizar el producto";
             }
         }
-        require 'views/almacen/edit.phtml';
+        require 'views/producto/edit.phtml';
     }
 
     public function delete($id) {
-        if ($this->model->deleteAlmacen($id)) {
-            header('Location: ' . BASE_URL . '/almacenes');
+        if ($this->model->deleteProducto($id)) {
+            header('Location: ' . BASE_URL . '/productos');
         } else {
-            $error = "Error al eliminar el almacén";
+            $error = "Error al eliminar el producto";
             require 'views/error.phtml';
         }
+    }
+
+    public function byAlmacen($almacenId) {
+        $productos = $this->model->getProductosByAlmacen($almacenId);
+        $almacen = $this->almacenModel->getAlmacenById($almacenId);
+        require 'views/producto/listByAlmacen.phtml';
     }
 }
