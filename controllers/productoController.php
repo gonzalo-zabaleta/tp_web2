0436836b4a03cca7e1399c6c5b9d401698882214
Gonzalo Zabaleta <gonzalo.zabaleta15@gmail.com>
2024-10-16 18:17:36 -0300

    Se agrego controlador productos con sus correspondientes funciones

---
+++ controllers/productoController.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'models/almacenModel.php';
+
+class AlmacenController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new AlmacenModel();
+    }
+
+    public function index() {
+        $almacenes = $this->model->getAllAlmacenes();
+        require 'views/almacen/list.phtml';
+    }
+
+    public function detail($id) {
+        $almacen = $this->model->getAlmacenById($id);
+        require 'views/almacen/detail.phtml';
+    }
+
+    public function add() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nombre = $_POST['nombre'];
+            $lugar = $_POST['lugar'];
+            $capacidad = $_POST['capacidad'];
+            if ($this->model->addAlmacen($nombre, $lugar, $capacidad)) {
+                header('Location: ' . BASE_URL . '/almacenes');
+            } else {
+                $error = "Error al agregar el almacén";
+            }
+        }
+        require 'views/almacen/add.phtml';
+    }
+
+    public function edit($id) {
+        $almacen = $this->model->getAlmacenById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nombre = $_POST['nombre'];
+            $lugar = $_POST['lugar'];
+            $capacidad = $_POST['capacidad'];
+            if ($this->model->updateAlmacen($id, $nombre, $lugar, $capacidad)) {
+                header('Location: ' . BASE_URL . '/almacenes');
+            } else {
+                $error = "Error al actualizar el almacén";
+            }
+        }
+        require 'views/almacen/edit.phtml';
+    }
+
+    public function delete($id) {
+        if ($this->model->deleteAlmacen($id)) {
+            header('Location: ' . BASE_URL . '/almacenes');
+        } else {
+            $error = "Error al eliminar el almacén";
+            require 'views/error.phtml';
+        }
+    }
+}
