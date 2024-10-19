a13decd8451b7700ecea6640261b0f534d22aa1c
Gonzalo Zabaleta <gonzalo.zabaleta15@gmail.com>
2024-10-19 16:00:28 -0300

    Se añadio el router

---
+++ libs/Router.php
@@ -0,0 +1,22 @@
+<?php
+class Router {
+    private $routes = [];
+
+    public function addRoute($url, $controller, $method) {
+        $this->routes[$url] = ['controller' => $controller, 'method' => $method];
+    }
+
+    public function route($url) {
+        if (array_key_exists($url, $this->routes)) {
+            $controller = $this->routes[$url]['controller'];
+            $method = $this->routes[$url]['method'];
+
+            $controllerInstance = new $controller();
+            $controllerInstance->$method();
+        } else {
+            // Handle 404 Not Found
+            header("HTTP/1.0 404 Not Found");
+            echo "404 Not Found";
+        }
+    }
+}
